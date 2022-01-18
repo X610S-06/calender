038de2f1153cd7a6b6afdeb2ff9240802c4a6470
Somanath1458 <37767811+Somanath1458@users.noreply.github.com>
2022-01-18 18:42:49 +0530

    "add custom date range"

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -3,10 +3,18 @@ import axios from 'axios'
 import moment from 'moment'
 
 const Calender = () => {
+	//initialisation
 	const [england, setEngland] = useState([])
 	const [scotland, setScotland] = useState([])
 	const [ireland, setIreland] = useState([])
+	const [customDate, setCustomDate] = useState({
+		from: '',
+		to: '',
+	})
+	const arr = [...england, ...scotland, ...ireland]
+	const [results, setResults] = useState([])
 
+	//fetching data
 	useEffect(() => {
 		axios('https://www.gov.uk/bank-holidays.json')
 			.then((response) => {
@@ -17,74 +25,135 @@ const Calender = () => {
 			.catch((error) => console.error(error))
 	}, [])
 
-	const arr = [...england, ...scotland, ...ireland]
-	// console.log(arr)
 	const date = new Date()
-
 	//handling yesterday filter
 	const handleYesterday = () => {
+		let array = []
 		const yesterday = moment(date).subtract(1, 'days').format('YYYY-MM-DD')
 		// console.log(yesterday)
-
+		console.log('Results for yesterday :')
 		arr.forEach((item) => {
-			if (moment(item.date).format('YYYY-MM-DD') === yesterday)
-				console.log(`Results for yesterday : ${item.date}`)
+			if (moment(item.date).format('YYYY-MM-DD') === yesterday) {
+				array.push(item)
+			}
+			setResults([...array])
 		})
 	}
 
 	//handling lastweek filter
 	const handleLastWeek = () => {
+		let array = []
 		const today = moment(date).format('YYYY-MM-DD')
-		// console.log(today)
 		const week = moment(date).subtract(7, 'days').format('YYYY-MM-DD')
-		// console.log(week)
-    console.log('Last Week dates :')
+
+		console.log('Results for last week :')
 		arr.forEach((item) => {
 			if (
 				moment(item.date).format('YYYY-MM-DD') <= today &&
 				moment(item.date).format('YYYY-MM-DD') >= week
-			)
-				console.log(`${item.title} ${item.date}`)
+			) {
+				array.push(item)
+			}
+			setResults([...array])
 		})
 	}
 
 	//handling lastmonth filter
 	const handleLastMonth = () => {
+		let array = []
 		const today = moment(date).format('YYYY-MM-DD')
-		// console.log(today)
 		const month = moment(date).subtract(30, 'days').format('YYYY-MM-DD')
-		// console.log(month)
-    console.log('Last Month dates are :')
-		arr.forEach((item) => {
+
+		console.log('Results for last month :')
+		arr.map((item) => {
 			if (
 				moment(item.date).format('YYYY-MM-DD') <= today &&
 				moment(item.date).format('YYYY-MM-DD') >= month
-			)
-				console.log(`${item.title} ${item.date}`)
+			) {
+				array.push(item)
+			}
+			setResults([...array])
+		})
+	}
+
+	//handling custom date range
+	const handleChange = (e) => {
+		e.preventDefault()
+		const { name, value } = e.target
+		console.log(name, value)
+		setCustomDate({
+			...customDate,
+			[name]: value,
+		})
+	}
+	console.log(customDate)
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		let array = []
+		console.log('Dates in the range are :')
+		arr.forEach((item) => {
+			if (
+				moment(item.date).format('YYYY-MM-DD') <= customDate.to &&
+				moment(item.date).format('YYYY-MM-DD') >= customDate.from
+			) {
+				array.push(item)
+			}
+			setResults([...array])
 		})
 	}
 
 	return (
-		<div className='flex justify-center items-center'>
-			{/* <table className='text-white'>
+		<div className='flex flex-col justify-center md:flex-row lg:flex-row xl:flex-row '>
+			<div className='p-7 md:p-20'>
+				<button className='border p-2 m-2 rounded' onClick={handleYesterday}>
+					Yesterday
+				</button>
+				<button className='border p-2 m-2 rounded' onClick={handleLastWeek}>
+					Last Week
+				</button>
+				<button className='border p-2 m-2 rounded' onClick={handleLastMonth}>
+					Last Month
+				</button>
+
+				<form onSubmit={handleSubmit} className='flex flex-col p-4 m-4'>
+					<label>From Date : </label>
+					<input
+						className='p-2 m-4 border-2 rounded'
+						type='date'
+						name='from'
+						placeholder='YYYY-MM-DD'
+						value={customDate.from}
+						onChange={handleChange}
+					/>
+					<label>To Date : </label>
+					<input
+						className='p-2 m-4 border-2 rounded'
+						type='date'
+						name='to'
+						placeholder='YYYY-MM-DD'
+						value={customDate.to}
+						onChange={handleChange}
+					/>
+					<button className='border w-24 p-2 mx-20 my-5 rounded'>SUBMIT</button>
+				</form>
+			</div>
+
+			<table className='text-white my-20'>
+				{/* <thead>
+					<tr className='flex flex-row justify-center'>
+						<th className='p-2 m-2'>Results :</th>
+					</tr>
+				</thead> */}
 				<tbody>
-					{arr.map((item, index) => (
-						<tr key={item.index} className='flex flex-row '>
+					{results.map((item, index) => (
+						<tr key={item.index} className='flex flex-row justify-center'>
 							<td className='p-2'>{item.title}</td>
 							<td className='p-2'>{item.date}</td>
 						</tr>
 					))}
 				</tbody>
-			</table> */}
-			<button className='border p-2 m-2' onClick={handleYesterday}>
-				Yesterday
-			</button>
-			<button className='border p-2 m-2' onClick={handleLastWeek}>
-				Last Week
-			</button>
-			<button className='border p-2 m-2' onClick={handleLastMonth}>
-				Last Month
-			</button>
+			</table>
 		</div>
 	)
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Header = () => {
+	const date = new Date()
+	
   return (
-		<div className='flex flex-col justify-center items-center my-12 text-6xl font-mono text-[#f7454d]'>
-			<h2>Calender</h2>
+		<div className='flex flex-col justify-center items-center my-12 text-5xl font-mono text-[#f7454d]'>
+			<h2>CALENDER</h2>
 		</div>
 	)
 }
